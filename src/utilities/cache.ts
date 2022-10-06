160e0214190db04d582be8cf6a6f143c6f5ee972
lee minhoon <hackids@naver.com>
2022-10-06 19:01:13 +0900

    writing...

--- src/utilities/cache.ts
+++ src/utilities/cache.ts
@@ -1,50 +1,68 @@
-import axios from "axios";
-
+/**
+ * 해당 이름을 가진 스토리지를 반환한다.
+ * 만들어져 있다면, 만들어진 스토리지를, 없다면 만들어서 반환한다.
+ * @param storageName
+ * @returns
+ */
 export const openCache = async (storageName: string): Promise<Cache> => {
   const storage = await window.caches.open(storageName);
   return storage;
 };
 
-export const addCache = async (storageName: string, requestUrl: string) => {
-  const storage = await openCache(storageName);
-  storage.add(requestUrl);
+export const putCache = async (
+  storageName: string,
+  url: string,
+  res: Response
+) => {
+  const storage = await window.caches.open(storageName);
+  storage.put(url, res);
 };
 
-export const matchCache = async (storageName: string, requestUrl: string) => {
+/**
+ * 스토리지안에 요청한 URL을 키로 가진 캐시 데이터가 있는지 확인한다.
+ * @param storageName
+ * @param url
+ * @returns 캐시 데이터가 없다면 undefined를 반환한다.
+ */
+export const matchCache = async (storageName: string, url: string) => {
   const storage = await openCache(storageName);
-  return storage.match(requestUrl);
+  return storage.match(url);
 };
 
-export const putCache = async (storageName: string, word, callback) => {
-  const url = `http://localhost:4000/sick?q=${word}`;
-  console.log("search...", word);
-  const exist = await matchCache(storageName, url);
-  if (exist) {
-    // 캐시 있으면
-    exist.json().then((res) => {
-      // 그대로 콜백에 쏴줌 (setData)
-      console.log("cache res:", res);
-      callback(res);
+/**
+ * 스토리지 안에 URL를 키로 가진 캐시 데이터를 삭제한다.
+ * @param storageName
+ * @param url
+ */
+export const deleteCache = async (storageName: string, url: string) => {
+  const storage = await openCache(storageName);
+  storage.delete(url);
+};
+
+/**
+ * 스토리지 안에 URL를 키로 가진 캐시 데이터가 있는지 확인 후,
+ * 있다면 캐시 데이터를 반환하고
+ * 없다면 API를 요청해 response를 반환한다.
+ * @param storageName
+ * @param requestUrl
+ * @param callback
+ */
+export const call = async <T>(
+  storageName: string,
+  requestUrl: string,
+  callback?: (res: T, resType: string) => void
+) => {
+  const cacheData = await matchCache(storageName, requestUrl);
+  if (cacheData) {
+    cacheData.json().then((res: T) => {
+      callback && callback(res, "cache data");
     });
   } else {
-    // 캐시없으면 fetch함 페치한거를 콜백에 쏴주고 (setData)
-    // 그리고 캐시에 넣음 addCache
-    fetch(url).then((res) => {
+    fetch(requestUrl).then((res) => {
       res.json().then((res) => {
-        console.log("api call res:", res);
-        callback(res);
+        callback && callback(res, "api response");
       });
-      addCache(storageName, url);
+      return putCache(storageName, requestUrl, res);
     });
   }
-  //   matchCache('cache', url).then(async (res) => {
-  //     if (res) {
-  //       console.log('hit cache');
-  //       const data = res.json();
-  //       return data;
-  //     } else {
-  //       console.log('api call');
-  //       return addCache('cache', url);
-  //     }
-  //   });
 };
